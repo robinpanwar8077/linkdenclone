dadf7a93b689064ebeda25be8a7b52cfd8060372
robinpanwar8077 <robinpanwar8058@gmail.com>
2024-09-16 12:05:33 +0530

    mainupdate

--- src/components/body/MainBody.js
+++ src/components/body/MainBody.js
@@ -1,83 +1,69 @@
-import React, {  useEffect, useState } from 'react'
-import Posts from './Posts'
-import {db,} from "../../firebaseConfig"
-import { serverTimestamp} from 'firebase/firestore'
-import {collection,addDoc, getDocs, query, orderBy} from "firebase/firestore"
-import CreatePost from './CreatePost'
-import {useSelector} from "react-redux"
+import React, { useEffect, useState, useCallback } from 'react';
+import Posts from './Posts';
+import { db } from "../../firebaseConfig";
+import { serverTimestamp } from 'firebase/firestore';
+import { collection, addDoc, getDocs, query, orderBy } from "firebase/firestore";
+import CreatePost from './CreatePost';
+import { useSelector } from "react-redux";
 
 function MainBody() {
-  const [posts,setPosts]=useState([])
-
-  const {name,profileUrl,email}=useSelector((store)=>(store.userDetails.user))
-
-  const getData=async()=>{
-    const sorted=query(collection(db,"userData"),orderBy("timeStamp",'desc'))
-    const snapshot=await getDocs(sorted);
-    setPosts(
-      snapshot.docs.map((doc)=>(
-          {
-            key:doc.id,
-            data:doc.data(),
-            profileUrl:profileUrl,
-            name:name,
-          }
-        )
-      )
-    )    
-  }
-
-  useEffect(()=>{
-    getData();
-    
-  },[])
-
-  const addData=async(data)=>{
-    try{
-      addDoc(collection(db,"userData"),data)
-        .then(()=>{
-          getData()
-        })
-    }
-    catch(e){
-      alert(e)
-    }
-  }
+  const [posts, setPosts] = useState([]);
   
+  const { name, profileUrl, email } = useSelector((store) => store.userDetails.user);
 
-  const handlePostCreation=(e,postContent)=>{
-    e.preventDefault()
+  const getData = useCallback(async () => {
+    const sorted = query(collection(db, "userData"), orderBy("timeStamp", 'desc'));
+    const snapshot = await getDocs(sorted);
+    setPosts(
+      snapshot.docs.map((doc) => ({
+        key: doc.id,
+        data: doc.data(),
+        profileUrl: profileUrl,
+        name: name,
+      }))
+    );
+  }, [profileUrl, name]); // Dependencies: memoize based on profileUrl and name
+
+  useEffect(() => {
+    getData();
+  }, [getData]); // Include getData as a dependency
+
+  const addData = async (data) => {
+    try {
+      await addDoc(collection(db, "userData"), data);
+      getData();
+    } catch (e) {
+      alert(e);
+    }
+  };
+
+  const handlePostCreation = (e, postContent) => {
+    e.preventDefault();
     addData({
-      nameDetails:email,
-      content:postContent,
-      timeStamp:serverTimestamp(),
-      profileUrl:profileUrl,
-      name:name,
-      
-    })
-    
-  }
+      nameDetails: email,
+      content: postContent,
+      timeStamp: serverTimestamp(),
+      profileUrl: profileUrl,
+      name: name,
+    });
+  };
 
-  const allPosts=posts.map((post)=>{
-    return(
-      <Posts
-        key={post.key}
-        name={post.data.name}
-        nameDetails={post.data.nameDetails}
-        content={post.data.content}
-        profileUrl={post.data.profileUrl}
-      />
-    )
-  })
+  const allPosts = posts.map((post) => (
+    <Posts
+      key={post.key}
+      name={post.data.name}
+      nameDetails={post.data.nameDetails}
+      content={post.data.content}
+      profileUrl={post.data.profileUrl}
+    />
+  ));
 
   return (
-    <div className="main--body w-full xl:w-7/12 flex flex-col gap-4">
-        <CreatePost
-          handlePostCreation={handlePostCreation}
-        />
-        {allPosts}
+    <div className="flex flex-col w-full gap-4 main--body xl:w-7/12">
+      <CreatePost handlePostCreation={handlePostCreation} />
+      {allPosts}
     </div>
-  )
+  );
 }
 
-export default MainBody
+export default MainBody;
